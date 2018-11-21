c040bfdddda377cdbf897a9bc05561c351f598e7
Todd <atodko0513@gmail.com>
2018-11-21 11:14:10 +0800

    Upload updated activity_category

--- app/src/main/java/com/app/todbayar/checklist/MainActivity.java
+++ app/src/main/java/com/app/todbayar/checklist/MainActivity.java
@@ -1,13 +1,25 @@
 package com.app.todbayar.checklist;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_category);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
     }
 }
