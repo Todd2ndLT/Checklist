0d6c865ca07af6ecb10d5df7b6d64a6cf6939545
Todd <atodko0513@gmail.com>
2018-11-21 17:55:10 +0800

    Upload updated activity_category

--- app/src/main/java/com/app/todbayar/checklist/MainActivity.java
+++ app/src/main/java/com/app/todbayar/checklist/MainActivity.java
@@ -1,21 +1,47 @@
 package com.app.todbayar.checklist;
 
 import android.support.annotation.NonNull;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private Toolbar toolbar;
+    private DrawerLayout layoutDrawer;
+    private NavigationView lSideMenu;
+    private Menu sideMenu;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        /*toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_drawer_menu);*/
+        //SETTING MENUS
+        /*layoutDrawer = (DrawerLayout) findViewById(R.id.view_main_layout);
+        lSideMenu = findViewById(R.id.view_main_menu);
+        lSideMenu.setNavigationItemSelectedListener(this);
+        sideMenu = (Menu) lSideMenu.getMenu();*/
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                layoutDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
